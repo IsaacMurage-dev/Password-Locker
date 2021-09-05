6bba4a69d65c4a5a4a27519cf8183ac9dbdd9e51
Your Name <you@example.com>
2021-09-06 00:21:29 +0300

    create functionality

--- run.py
+++ run.py
@@ -2,3 +2,6 @@ from user import User
 from credential import Credentials
 from random import randint
 '''for the user class'''
+def createUser(username, password):
+    myUser = User(username, password)
+    return myUser
