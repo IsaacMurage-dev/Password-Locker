51d0d8d828712ca0af037728e9c5895721523b08
Your Name <you@example.com>
2021-09-06 01:48:54 +0300

    modify run.py

--- run.py
+++ run.py
@@ -18,3 +18,15 @@ def saveCredentials(credentials):
     credentials.saveCred()
 def displayCredentials():
     Credentials.displayCredentials()
+
+def main():
+    print("Welcome to your password locker,")
+    print("\n")
+    while True: 
+        shortcodes= input("To create Account,type either cu or du: cu to create user : du to display user:").lower()
+        if shortcodes == "cu":
+            username = input("Enter your preffered username:")
+            password = input("Enter your preffered  password:")
+            saveUser(createUser(username, password))
+            print(f"Hello {username} thank you for creating your password locker account, you can now proceed")
+            print("\n")
