e00e672deca4264de38a8f9e9281904b960bfae3
Your Name <you@example.com>
2021-09-06 02:04:34 +0300

    add create account and login function

--- run.py
+++ run.py
@@ -30,3 +30,18 @@ def main():
             saveUser(createUser(username, password))
             print(f"Hello {username} your password locker account has been created, you can now proceed")
             print("\n")
+
+            while True:
+                print(f"Welcome {username} to the section of adding accounts and their passwords:")
+                print("-"*20)
+                code =input("Please choose the following to interact with the application:  cc: to create credentials: dc to display credentials:").lower()
+                if code == "cc":
+                    feedback = input("Enter y to create your own password and n for the system to generate:").lower()
+                    if feedback == ("y"):
+                        account = input("Enter the name of account, eg facebook, twitter:")
+                        user_name = input("Enter your desired username:")
+                        pass_word = input("Enter the password:")
+                        saveCredentials(createCredentials(account,user_name, pass_word))
+                        print("Credentials uploaded successfully")
+
+main()
